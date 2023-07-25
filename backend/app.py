43b6ff16904bf0e7b346d03d133a818bb221623f
hvrazel <vrazel97@yahoo.com>
2023-07-25 16:08:37 -0500

    updated main function port details

--- backend/app.py
+++ backend/app.py
@@ -270,4 +270,6 @@ def sendEmail():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))  # Default port is 5000 if PORT is not set
+    app.run(debug=True, host='0.0.0.0', port=port)
+
